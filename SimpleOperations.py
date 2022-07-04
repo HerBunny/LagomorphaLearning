b252478f26fd089fd6098a4a3c714f54a9ff731d
IainO81 <101501891+HerBunny@users.noreply.github.com>
2022-07-04 14:59:16 +0200

    Why, I, Aughtta!!!

--- SimpleOperations.py
+++ SimpleOperations.py
@@ -1,6 +1,6 @@
 # Simple Operations
 
 # This is a program to determine the total number of scoops of ice cream needed, to serve two 
-# scoops each to 68 children.
+# scoops each, to 68 children.
 
 print(68 * 2)
